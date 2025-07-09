decc9ac892bc1c21fa3439fbcab81b39232b7dae
Chong <chongjinjye@gmail.com>
2025-07-09 14:51:14 +0800

    update

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def upload_to_databricks(local_file_path, host, token):
 
 
 @flow
-def upload_to_databricks_flow(host: str = " ", token: str = " "):
+def upload_to_databricks_flow(host: str, token: str):
     try:
         upload_to_databricks(audio_file, host, token)
     except Exception as e:
